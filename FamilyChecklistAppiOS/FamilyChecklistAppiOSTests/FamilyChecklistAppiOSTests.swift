449397ab95287f2c78570d40b7a44cb8157d1fc4
Benjamin james cawley <>
2025-07-29 13:34:19 +1200

    Boilerplate set up and planing

---
+++ FamilyChecklistAppiOS/FamilyChecklistAppiOSTests/FamilyChecklistAppiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyChecklistAppiOSTests.swift
+//  FamilyChecklistAppiOSTests
+//
+//  Created by Benjamin james cawley on 29/07/2025.
+//
+
+import Testing
+@testable import FamilyChecklistAppiOS
+
+struct FamilyChecklistAppiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

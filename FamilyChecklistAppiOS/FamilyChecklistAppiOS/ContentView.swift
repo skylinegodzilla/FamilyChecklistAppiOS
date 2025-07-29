c4e49000377e8fd749f4ae5e4e7ef8581f824759
Benjamin james cawley <>
2025-07-29 13:34:19 +1200

    Boilerplate set up and planing

---
+++ FamilyChecklistAppiOS/FamilyChecklistAppiOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FamilyChecklistAppiOS
+//
+//  Created by Benjamin james cawley on 29/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
